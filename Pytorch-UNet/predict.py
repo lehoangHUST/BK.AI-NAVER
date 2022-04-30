f762e8adc92b7b3637da4e028496dbdf94f22f3f
lehoangHUST <lehoangEDABK@gmail.com>
2022-04-30 22:28:20 +0700

    Update code predict.py

--- Pytorch-UNet/predict.py
+++ Pytorch-UNet/predict.py
@@ -5,6 +5,7 @@ import os
 import numpy as np
 import torch
 import csv
+import cv2
 import torch.nn.functional as F
 from PIL import Image
 from torchvision import transforms
@@ -12,6 +13,7 @@ from torchvision import transforms
 from utils.data_loading import BasicDataset
 from unet import UNet
 from utils.utils import plot_img_and_mask
+
 
 def predict_img(net,
                 full_img,
@@ -49,10 +51,10 @@ def get_args():
     parser = argparse.ArgumentParser(description='Predict masks from input images')
     parser.add_argument('--model', '-m', default='MODEL.pth', metavar='FILE',
                         help='Specify the file in which the model is stored')
-    parser.add_argument('--input', default='/content/test', type=str, help='Filenames of input images', required=True)
-    parser.add_argument('--output', '-o', metavar='OUTPUT', nargs='+', help='Filenames of output images')
-    parser.add_argument('--viz', '-v', action='store_true',
-                        help='Visualize the images as they are processed')
+    parser.add_argument('--video', type=str, default=None, help='Save video .avi form input')
+    parser.add_argument('--videos', type=str, default=None, help='Save folder video .avi form folder input')
+    parser.add_argument('--image', type=str, default=None, help='Save image form input')
+    parser.add_argument('--images', type=str, default=None, help='Save folder image form folder input')
     parser.add_argument('--no-save', '-n', action='store_true', help='Do not save the output masks')
     parser.add_argument('--mask-threshold', '-t', type=float, default=0.5,
                         help='Minimum probability value to consider a mask pixel white')
@@ -84,15 +86,15 @@ def total_pixel(mask):
     count_max = 0
     mask = np.array(mask)
     print(mask.shape)
-    for i in range (mask.shape[0]):
+    for i in range(mask.shape[0]):
         for j in range(mask.shape[1]):
             if mask[i, j] == 255:
                 count_max += 1
             if mask[i, j] == 0:
                 count_zero += 1
-    
-    print(f"Total pixel have value zero is {count_zero} \n"
-          f"Total pixel have value max (255) is {count_max}.")
+
+    print(count_max)
+    print(count_zero)
 
 
 # Encode image mask to file csv
@@ -111,7 +113,7 @@ def rle_encode(img):
 def rle_decode(mask_rle, shape):
     '''
     mask_rle: run-length as string formated (start length)
-    shape: (height,width) of array to return 
+    shape: (height,width) of array to return
     Returns numpy array, 1 - mask, 0 - background
 
     '''
@@ -119,16 +121,107 @@ def rle_decode(mask_rle, shape):
     starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
     starts -= 1
     ends = starts + lengths
-    img = np.zeros(shape[0]*shape[1], dtype=np.uint8)
+    img = np.zeros(shape[0] * shape[1], dtype=np.uint8)
     for lo, hi in zip(starts, ends):
         img[lo:hi] = 1
     return img.reshape(shape)
 
 
+# Run multiple video
+def evaluate_videos(net_yolact, path: str, save_path: str):
+    if not os.path.isdir(save_path):
+        os.mkdir(save_path)
+
+    for vid in os.listdir(path):
+        evaluate_video(net_yolact, os.path.join(path, vid), os.path.join(save_path, vid))
+
+
+# Run video
+def evaluate_video(net_yolact, path: str, save_path: str):
+    vid = cv2.VideoCapture(path)
+
+    if not vid.isOpened():
+        print('Could not open video "%s"' % path)
+        exit(-1)
+
+    target_fps = round(vid.get(cv2.CAP_PROP_FPS))
+    frame_width = round(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = round(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    # this format fail to play in Chrome/Win10/Colab
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V')  # codec
+    # fourcc = cv2.VideoWriter_fourcc(*'H264') #codec
+    output = cv2.VideoWriter(save_path, fourcc, target_fps, (frame_width, frame_height))
+
+    frame = None
+    while True:
+
+        try:
+            is_success, frame = vid.read()
+        except cv2.error:
+            continue
+
+        if not is_success:
+            break
+
+        img = cv2.resize(frame, (256, 256))
+        img = Image.fromarray(img)
+        mask = predict_img(net=net,
+                           full_img=img,
+                           scale_factor=args.scale,
+                           out_threshold=args.mask_threshold,
+                           device=device)
+
+        result = np.array(mask_to_image(mask))
+        result = result.reshape(result.shape[0], result.shape[1], 1)
+        result = cv2.resize(result, (frame_width, frame_height))
+        mask = cv2.cvtColor(result, cv2.COLOR_GRAY2RGB)
+        # OPTIONAL: do some processing
+        # convert cv2 BGR format to RGB
+        # Path image or image: to run one image/ list image or video
+        output.write(mask)
+    # OPTIONAL: show last image
+    vid.release()
+
+
+def evaluate_image(net, path: str, save_path: str):
+    img = Image.open(path)
+    mask = predict_img(net=net,
+                       full_img=img,
+                       scale_factor=args.scale,
+                       out_threshold=args.mask_threshold,
+                       device=device)
+    result = np.array(mask_to_image(mask))
+    result = result.reshape(result.shape[0], result.shape[1], 1)
+    cv2.imwrite(save_path, result)
+
+
+def evaluate_images(net, path: str, save_path: str):
+    if not os.path.isdir(save_path):
+        os.makedirs(save_path)
+
+    for filename in os.listdir(path):
+        evaluate_image(net, os.path.join(path, filename), os.path.join(save_path, filename))
+
+
+def evaluate(net, args):
+    # Run video
+    if args.video is not None:
+        inp, out = args.video.split(':')
+        evaluate_video(net, inp, out)
+    elif args.videos is not None:
+        inp, out = args.videos.split(':')
+        evaluate_videos(net, inp, out)
+    elif args.image is not None:
+        inp, out = args.image.split(':')
+        evaluate_image(net, inp, out)
+    elif args.images is not None:
+        inp, out = args.images.split(':')
+        evaluate_images(net, inp, out)
+
+
 if __name__ == '__main__':
     args = get_args()
-    in_files = args.input
-    out_files = get_output_filenames(args)
 
     net = UNet(n_channels=3, n_classes=2, bilinear=args.bilinear)
 
@@ -141,32 +234,4 @@ if __name__ == '__main__':
 
     logging.info('Model loaded!')
 
-    results = []
-    for i, filename in enumerate(os.listdir(in_files)):
-        logging.info(f'\nPredicting image {filename} ...')
-        img = Image.open(os.path.join(in_files, filename))
-
-        mask = predict_img(net=net,
-                           full_img=img,
-                           scale_factor=args.scale,
-                           out_threshold=args.mask_threshold,
-                           device=device)
-
-        if not args.no_save:
-            result = np.array(mask_to_image(mask))
-            result = result.reshape(result.shape[0], result.shape[1], 1)
-            r = rle_encode(result)
-
-            # Append 
-            results.append([0, filename.split('/')[-1], r])
-
-        if args.viz:
-            logging.info(f'Visualizing results for image {filename}, close to continue...')
-            plot_img_and_mask(img, mask)
-
-    # Save file 
-    with open(args.save_csv, 'w', encoding='UTF8', newline='') as f:
-        writer = csv.writer(f)
-
-        # write multiple rows
-        writer.writerows(results)
+    evaluate(net, args)
